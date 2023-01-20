95ae68a581fbad402053c20d51da234b6f95209c
TanyaTash <tanya.tash0@gmail.com>
2023-01-20 12:08:53 +0300

    Added homework lesson 1

---
+++ lesson_1/task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число:");
+int firstNumber = int.Parse(Console.ReadLine() ?? string.Empty);
+Console.WriteLine("Введите второе число:");
+int secondNumber = int.Parse(Console.ReadLine() ?? string.Empty);
+
+Console.WriteLine(firstNumber > secondNumber ? $"Число {firstNumber} больше" : $"Число {secondNumber} больше");
---
+++ lesson_1/task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите числа через запятую:");
+string[] numbers = Console.ReadLine().Split(',');
+int firstNumber = int.Parse(numbers[0]);
+
+foreach (var number in numbers)
+{
+    int ecahNumber = int.Parse(number);
+    if (ecahNumber > firstNumber)
+    {
+        firstNumber = ecahNumber;
+    };
+};
+System.Console.WriteLine($"Самое большое число: {firstNumber}");
