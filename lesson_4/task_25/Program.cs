44164ba279a93a555a5087b41dc1b46b2a3f13c9
TanyaTash <tanya.tash0@gmail.com>
2023-01-28 11:09:38 +0300

    Added homework lesson 4

---
+++ lesson_4/task_25/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+do
+{
+    try
+    {
+        Console.Write("Введите число для возведения в степень: ");
+        int numberFirst = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите натуральное число для степени: ");
+        int numberSecond = Convert.ToInt32(Console.ReadLine());
+
+        if (numberSecond > 0)
+        {
+            int result = numberFirst;
+
+            for (int i = 1; i < numberSecond; i++)
+            {
+                result = result * numberFirst;
+            }
+
+            Console.WriteLine($"Результат циклом: {result}");
+            Console.WriteLine($"Результат функцией Math.Pow: {Math.Pow(numberFirst, numberSecond)}");
+        } else Console.WriteLine("Вы ввели неверное число");
+
+        break;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Неверный формат ввода. Введите натуральное число");
+    }
+}
+while (true);
---
+++ lesson_4/task_27/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите положительное число: ");
+do
+{
+    try
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+        int[] numberArray = number.ToString().Select(o => Convert.ToInt32(o) - 48).ToArray();
+        int result = 0;
+
+        for (int i = 0; i < numberArray.Length; i++)
+        {
+
+            result = result + numberArray[i];
+
+        }
+        Console.WriteLine($"Результат равен: {result}");
+        break;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Неверный формат ввода. Введите действительное положительное число");
+    }
+}
+while (true);
