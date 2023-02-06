d095ac76b2ee01b9303f04b25a4fb516b32339d9
TanyaTash <tanya.tash0@gmail.com>
2023-02-06 18:34:42 +0300

    Added homework lesson 7

---
+++ lesson_7/task_47/Program.cs
@@ -0,0 +1,65 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+Console.Clear();
+void RandomNums()
+{
+    do
+    {
+        try
+        {
+
+            Console.WriteLine("Для заполнения массива случайными числами заполните параметры ниже.");
+            int lineLenght = InputValues("Введите количество строк: ");
+            int columnLenght = InputValues("Введите количество столбцов: ");
+
+            double[,] numArray = new double[lineLenght, columnLenght];
+
+            int InputValues(string label)
+            {
+                Console.Write($"{label}");
+                return Convert.ToInt32(Console.ReadLine());
+            }
+
+            RandomNumbers(numArray);
+            ArrayOutput(numArray);
+
+            void RandomNumbers(double[,] array)
+            {
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        array[i, j] = Convert.ToDouble(new Random().Next(-128, 128)) / 10;
+                    }
+                }
+            }
+
+            void ArrayOutput(double[,] array)
+            {
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    Console.Write("[ ");
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        Console.Write(array[i, j] + " ");
+                    }
+                    Console.Write("]");
+                    Console.WriteLine("");
+                }
+            }
+            break;
+        }
+        catch (FormatException)
+        {
+            Console.Clear();
+            Console.WriteLine("Неверный формат ввода. Введите действительное положительное число");
+        }
+    }
+    while (true);
+}
+
+RandomNums();
---
+++ lesson_7/task_50/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.Clear();
+void CheckArray()
+{
+    do
+    {
+        try
+        {
+            Console.WriteLine("Для заполнения массива случайными числами заполните параметры ниже.");
+            int lineLenght = InputValues("Введите количество строк: ");
+            int columnLenght = InputValues("Введите количество столбцов: ");
+
+            double[,] numArray = new double[lineLenght, columnLenght];
+
+            int InputValues(string label)
+            {
+                Console.Write($"{label}");
+                return Convert.ToInt32(Console.ReadLine());
+            }
+
+            RandomNumbers(numArray);
+            ArrayOutput(numArray);
+
+            void RandomNumbers(double[,] array)
+            {
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        array[i, j] = Convert.ToDouble(new Random().Next(-128, 128)) / 10;
+                    }
+                }
+            }
+
+            void ArrayOutput(double[,] array)
+            {
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    Console.Write("[ ");
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        Console.Write(array[i, j] + " ");
+                    }
+                    Console.Write("]");
+                    Console.WriteLine("");
+                }
+            }
+
+            int row = InputValues("Введите индекс строки: ");
+            int col = InputValues("Введите индекс столбца: ");
+
+            if (row < 0 || col < 0 || row > numArray.GetLength(0) || col > numArray.GetLength(1))
+            {
+                Console.WriteLine("По заданному индексу не было найдено ни одного значения");
+            }
+            else
+            {
+                Console.WriteLine(numArray[row, col]);
+            }
+            break;
+        }
+        catch (FormatException)
+        {
+            Console.Clear();
+            Console.WriteLine("Неверный формат ввода. Введите действительное положительное число");
+        }
+    }
+    while (true);
+}
+
+CheckArray();
